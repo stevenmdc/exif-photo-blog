388a6ed24a6802b1a7310c5c7663eaf6b3526307
Baggi4 <stevenmadec@live.fr>
2024-05-14 14:57:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/services/openai.ts
@@ -0,0 +1,74 @@
+'use server';
+
+import { streamText } from 'ai';
+import { createStreamableValue } from 'ai/rsc';
+import { createOpenAI } from '@ai-sdk/openai';
+import { kv } from '@vercel/kv';
+import { Ratelimit } from '@upstash/ratelimit';
+import { AI_TEXT_GENERATION_ENABLED, HAS_VERCEL_KV } from '@/site/config';
+import { safelyRunAdminServerAction } from '@/auth';
+import { removeBase64Prefix } from '@/utility/image';
+
+const RATE_LIMIT_IDENTIFIER = 'openai-image-query';
+const RATE_LIMIT_MAX_QUERIES_PER_HOUR = 100;
+
+const openai = AI_TEXT_GENERATION_ENABLED
+  ? createOpenAI({ apiKey: process.env.OPENAI_SECRET_KEY })
+  : undefined;
+
+// Allows 100 requests per hour
+const ratelimit = HAS_VERCEL_KV
+  ? new Ratelimit({
+    redis: kv,
+    limiter: Ratelimit.slidingWindow(RATE_LIMIT_MAX_QUERIES_PER_HOUR, '1h'),
+  })
+  : undefined;
+
+export const streamOpenAiImageQuery = async (
+  imageBase64: string,
+  query: string,
+) => {
+  return safelyRunAdminServerAction(async () => {
+    if (ratelimit) {
+      let success = false;
+      try {
+        success = (await ratelimit.limit(RATE_LIMIT_IDENTIFIER)).success;
+      } catch (e: any) {
+        console.error('Failed to rate limit OpenAI', e);
+        throw new Error('Failed to rate limit OpenAI');
+      }
+      if (!success) {
+        console.error('OpenAI rate limit exceeded');
+        throw new Error('OpenAI rate limit exceeded');
+      }
+    }
+
+    const stream = createStreamableValue('');
+
+    if (openai) {
+      (async () => {
+        const { textStream } = await streamText({
+          model: openai('gpt-4-vision-preview'),
+          messages: [{
+            'role': 'user',
+            'content': [
+              {
+                'type': 'text',
+                'text': query,
+              }, {
+                'type': 'image',
+                'image': removeBase64Prefix(imageBase64),
+              },
+            ],
+          }],
+        });
+        for await (const delta of textStream) {
+          stream.update(delta);
+        }
+        stream.done();
+      })();
+    }
+
+    return stream.value;
+  });
+};
---
+++ src/site/CommandK.tsx
@@ -0,0 +1,66 @@
+import CommandKClient, { CommandKSection } from '@/components/CommandKClient';
+import {
+  getPhotosCountCached,
+  getUniqueCamerasCached,
+  getUniqueFilmSimulationsCached,
+  getUniqueTagsCached,
+} from '@/photo/cache';
+import {
+  pathForCamera,
+  pathForFilmSimulation,
+} from './paths';
+import { formatCameraText } from '@/camera';
+import { photoQuantityText } from '@/photo';
+import { formatCount, formatCountDescriptive } from '@/utility/string';
+import { TagsWithMeta } from '@/tag';
+import PhotoFilmSimulationIcon from '@/simulation/PhotoFilmSimulationIcon';
+import { IoMdCamera } from 'react-icons/io';
+import { ADMIN_DEBUG_TOOLS_ENABLED } from './config';
+
+export default async function CommandK() {
+  const [
+    count,
+    tags,
+    cameras,
+    filmSimulations,
+  ] = await Promise.all([
+    getPhotosCountCached().catch(() => 0),
+    getUniqueTagsCached().catch(() => [] as TagsWithMeta),
+    getUniqueCamerasCached().catch(() => []),
+    getUniqueFilmSimulationsCached().catch(() => []),
+  ]);
+
+  const SECTION_CAMERAS: CommandKSection = {
+    heading: 'Cameras',
+    accessory: <IoMdCamera />,
+    items: cameras.map(({ camera, count }) => ({
+      label: formatCameraText(camera),
+      annotation: formatCount(count),
+      annotationAria: formatCountDescriptive(count),
+      path: pathForCamera(camera),
+    })),
+  };
+
+  const SECTION_FILM: CommandKSection = {
+    heading: 'Film Simulations',
+    accessory: <span className="w-3">
+      <PhotoFilmSimulationIcon className="translate-y-[0.5px]" />
+    </span>,
+    items: filmSimulations.map(({ simulation, count }) => ({
+      label: simulation,
+      annotation: formatCount(count),
+      annotationAria: formatCountDescriptive(count),
+      path: pathForFilmSimulation(simulation),
+    })),
+  };
+
+  return <CommandKClient
+    tags={tags}
+    serverSections={[
+      SECTION_CAMERAS,
+      SECTION_FILM,
+    ]}
+    showDebugTools={ADMIN_DEBUG_TOOLS_ENABLED}
+    footer={photoQuantityText(count, false)}
+  />;
+}
